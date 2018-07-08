67ff3ea2cb89d2ce22d6fe937e1d1e63208d6552
Chris Pezley <chris@pezley.net>
2018-07-08 14:14:00 +0200

    Use numpy to process data, which is much faster.

--- manip.py
+++ manip.py
@@ -3,8 +3,8 @@ import os
 import time
 import tqdm
 import multiprocessing as mp
-from bitstring import ConstBitStream
 from PIL import Image
+import numpy
 
 # 70
 # 80
@@ -17,40 +17,19 @@ from PIL import Image
 #STAMP = "749113"
 STAMP = "1297209"
 
-def handleDump(fileObj):
-    tech = []
-    raw = ConstBitStream(fileObj)
-    try:
-        while True:
-            tech.append(raw.read('floatle:32'))
-    except:
-        pass
-    del(raw)
-    return tech 
-
 
 def processing(params):
-    pStamp = params[0]
-    pI = params[1]
-    pJ = params[2]
-    VER = params[3]
-    HOR = params[4]
-    PXLS = params[5]
-    
-    filen = open( 'SAMPLE_{0}_{1}_{2}.dmp'.format(pStamp, str(pI), str(pJ) ), 'rb')
-    
+    pStamp, pI, pJ, VER, HOR, _ = params
+
     ## put data in RAM
-    tech = handleDump(filen)
-        
-    filen.close()
-    del(filen)
+    filename = 'SAMPLE_{0}_{1}_{2}.dmp'.format(pStamp, str(pI), str(pJ) )
+    tech = numpy.fromfile(filename, numpy.float32)
 
     ## DATA MANIP START
-    average = sum(tech)/float(len(tech))
-    
+    average = numpy.average(tech)
     colour = int( (average/100)*255 )
+
     hold = [ pStamp, pI, pJ, VER, HOR, colour ]
-    del(tech)
     return hold
     #print("{0} : [{1},{2}]".format(average,pI,pJ)) # DEBUG
 
